6287a1a4603cfb14e8214614efca1d96f2405b28
Christian Sommer <christian.sommer@gmx.ch>
2012-02-21 21:49:20 +0100

    standardized JavaScript file inclusion

--- backend_body.js
+++ backend_body.js
@@ -5,71 +5,21 @@
  * Add-ons such as modules, templates and languages via the Website Baker backend. This enables
  * you to perform small modifications on installed Add-ons without downloading the files first.
  *
- * This file contains the JS call required for the syntax highlighting with EditArea.
+ * This file includes the AFE JavaScript code required to toggle the Add-on overview sections.
  * 
  * LICENSE: GNU General Public License 3.0
  * 
  * @platform    CMS Websitebaker 2.8.x
  * @package     addon-file-editor
  * @author      cwsoft (http://cwsoft.de)
- * @version     2.0.0
+ * @version     2.2.0
  * @copyright   cwsoft
  * @license     http://www.gnu.org/licenses/gpl.html
 */
 
-// functions for the jQuery framework
-$(document).ready(function() {
-	// only execute on overview page
-	if ($('#trigger_modules,#trigger_templates,#trigger_languages').html() == null) return;
-	
-	// remove navigation boxes except the very first one (#modules)
-	$('.box:not("#modules")').hide();
-	
-	// remove links from first navigation box except the first link (Reload)
-	$('#modules a:not(:first)').hide();
-
-	// set default or user defined view mode settings
-	setViewMode('modules');
-	setViewMode('templates');
-	setViewMode('languages');
-
-	$('#trigger_modules, #trigger_templates, #trigger_languages')
-		.toggle(
-			function() {
-				var id = $(this).attr('id').replace(/trigger_/, '');
-				document.cookie = 'AFE_' + id + '=1';
-				setViewMode(id);
-			},
-			
-			function() {
-				var id = $(this).attr('id').replace(/trigger_/, '');
-				document.cookie = 'AFE_' + id + '=0';
-				setViewMode(id);
-			}
-		);
-
-});
-
-function setViewMode(id) {
-	// get class (expanded/collapsed)
-	var trigger = $('#trigger_' + id);
-	var view = $('#toggle_' + id)
-
-	// initialize trigger
-	if (trigger.attr('class') == '' && document.cookie.indexOf('AFE_' + id) == -1) {
-		// initialize triggers (set collapsed)
-		trigger.addClass('expanded');
-		view.addClass('hidden');
-		document.cookie = 'AFE_' + id + '=0';
-
-	} else if (document.cookie.indexOf('AFE_' + id + '=1') > -1) {
-		// toggle from collapsed to expanded
-		trigger.removeClass('expanded').addClass('collapsed');
-		view.removeClass('hidden');
-		
-	} else {
-		// toggle from expanded to collapsed
-		trigger.removeClass('collapsed').addClass('expanded');
-		view.addClass('hidden');
-	}
+// check if jQuery is loaded in the WebsiteBaker backend
+if (typeof jQuery != 'undefined' && typeof(WB_URL) != "undefined") {
+	// include JavaScript code to toggle the addon overview sections
+	$.getScript(WB_URL + "/modules/addon_file_editor/javascript/addon-file-editor.js", function(){
+	});
 }
--- javascript/index.php
+++ javascript/index.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * Admin tool: Addon File Editor
  *
@@ -5,20 +6,17 @@
  * Add-ons such as modules, templates and languages via the Website Baker backend. This enables
  * you to perform small modifications on installed Add-ons without downloading the files first.
  *
- * This file contains the legacy syntax highlighting support for Website Baker versionb below 2.8.
- * To get syntax highlighting working with Addon File Editor v0.80 and WB < 2.8, copy the folder
- * /include/editarea from a WB 2.8 installation package to the /include folder of your installation.
- * The rename this file from backend_legacy.js into backend.js and you are done.
+ * This file prevents directory listing.
  * 
  * LICENSE: GNU General Public License 3.0
  * 
  * @platform    CMS Websitebaker 2.8.x
  * @package     addon-file-editor
  * @author      cwsoft (http://cwsoft.de)
- * @version     2.0.0
+ * @version     2.2.0
  * @copyright   cwsoft
  * @license     http://www.gnu.org/licenses/gpl.html
 */
 
-// include the editarea framework
-document.write('<script type="text/javascript" src="../../include/editarea/edit_area_full.js"></script>');
+// prevent this file from being accessed directly
+if(defined('WB_PATH') == false) { exit("Cannot access this file directly"); }
